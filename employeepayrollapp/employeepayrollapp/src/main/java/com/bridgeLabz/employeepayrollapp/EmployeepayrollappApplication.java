4a0e8f60176a3ab41ae21ca124216f31b23aa6c6
Deepak20singh <137925992+Deepak20singh@users.noreply.github.com>
2025-02-25 16:19:15 +0530

    [Deepak] Add. Code of fetching data or information using application properties at run time UC8

--- employeepayrollapp/employeepayrollapp/src/main/java/com/bridgeLabz/employeepayrollapp/EmployeepayrollappApplication.java
+++ employeepayrollapp/employeepayrollapp/src/main/java/com/bridgeLabz/employeepayrollapp/EmployeepayrollappApplication.java
@@ -1,22 +1,20 @@
 package com.bridgeLabz.employeepayrollapp;
 
 
+import org.springframework.context.ApplicationContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
 
 public class EmployeepayrollappApplication {
-	private static final Logger log=  LoggerFactory.getLogger(EmployeepayrollappApplication.class);
+	private static final Logger log = LoggerFactory.getLogger(EmployeepayrollappApplication.class);
 
 	public static void main(String[] args) {
-
-		SpringApplication.run(EmployeepayrollappApplication.class, args);
-		log.info("Application has started");
+		ApplicationContext context = SpringApplication.run(EmployeepayrollappApplication.class, args);
+		log.info("Application has started in {} environment",
+				context.getEnvironment().getProperty("environment"));
 	}
-
 }
