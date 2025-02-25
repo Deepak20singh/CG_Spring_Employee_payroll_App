1a92e21ec6202d507e1b3d64e13044723bc27e06
Deepak20singh <137925992+Deepak20singh@users.noreply.github.com>
2025-02-25 10:40:47 +0530

    Delete extra part

--- employeepayrollapp/employeepayrollapp/src/main/java/tempory/repotemp.java
+++
@@ -1,8 +0,0 @@
-package tempory;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface repotemp extends JpaRepository<temporyEmployee,Long> {
-}
