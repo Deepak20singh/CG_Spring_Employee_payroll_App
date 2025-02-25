7becac8105a30283f19eab5c775984d6c0e47286
Deepak20singh <137925992+Deepak20singh@users.noreply.github.com>
2025-02-25 13:27:29 +0530

    [Deepak] Add. Code of Logger UC7

--- employeepayrollapp/employeepayrollapp/src/main/java/com/bridgeLabz/employeepayrollapp/EmployeepayrollappApplication.java
+++ employeepayrollapp/employeepayrollapp/src/main/java/com/bridgeLabz/employeepayrollapp/EmployeepayrollappApplication.java
@@ -1,13 +1,22 @@
 package com.bridgeLabz.employeepayrollapp;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
+
 public class EmployeepayrollappApplication {
+	private static final Logger log=  LoggerFactory.getLogger(EmployeepayrollappApplication.class);
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EmployeepayrollappApplication.class, args);
+		log.info("Application has started");
 	}
 
 }
