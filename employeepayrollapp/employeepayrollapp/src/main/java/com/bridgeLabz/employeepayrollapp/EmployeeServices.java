8a9d1a3e2a3ec08eed86d2802a4450d428fb3a68
Deepak20singh <137925992+Deepak20singh@users.noreply.github.com>
2025-02-27 16:37:18 +0530

    [Deepak] Add. Code of adding data into database UC15

--- employeepayrollapp/employeepayrollapp/src/main/java/com/bridgeLabz/employeepayrollapp/EmployeeServices.java
+++ employeepayrollapp/employeepayrollapp/src/main/java/com/bridgeLabz/employeepayrollapp/EmployeeServices.java
@@ -1,12 +1,13 @@
 package com.bridgeLabz.employeepayrollapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
+@Slf4j
 @Service
 public class EmployeeServices {
     private List<Employee> list=new ArrayList<>();
@@ -14,8 +15,11 @@ public class EmployeeServices {
 @Autowired
 private EmployeeRepository employeeRepository;
     public Employee add(Employee employee){
+        Employee emp=null;
+        emp=new Employee(employee);
+
         list.add(employee);
-        return employeeRepository.save(employee);
+        return employeeRepository.save(emp);
          }
 
     public boolean deleting(Long id) {
